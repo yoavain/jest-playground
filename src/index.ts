95f3362814e5bcf7be8c5e92e6db61414d159f31
Yoav Vainrich <yoavain@gmail.com>
2023-04-03 20:40:24 +0300

    multiply

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-export * from "./main";
+export * from "./multiply";
+export * from "./sum";
--- test/main.test.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from "../src/main";
-
-describe("Test main", () => {
-    it("Test 1+1", () => {
-        expect(sum(1, 1)).toEqual(2);
-    });
-});
